71e051271aa1779bfc47b6536b48c8652a33dfef
Andrea772004 <andrea2004854@gmail.com>
2022-11-20 17:44:25 +0100

    Buttons e funzione

---
+++ Sito/Static/JS/Index.js
@@ -0,0 +1,18 @@
+function ChangeColor1() {
+    document.body.style.backgroundColor = "#FE0202";
+ }
+ function ChangeColor2() {
+    document.body.style.backgroundColor = "#42f9b0";
+ }
+ function ChangeColor3() {
+   document.body.style.backgroundColor = "#0484d9";
+}
+function ChangeColor4() {
+   document.body.style.backgroundColor = "#82f858";
+}
+function ChangeColor5() {
+   document.body.style.backgroundColor = "#f442df";
+}
+function ChangeColor6() {
+   document.body.style.backgroundColor = "#f7f429";
+}
